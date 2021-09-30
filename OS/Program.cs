5cc830adca9d3b60364bd32d9f4fb613efaf35f8
Prikhod322 <vovkaprikhod@gmail.com>
2021-09-30 23:40:47 +0300

    File sorting

--- OS/Program.cs
+++ OS/Program.cs
@@ -1,29 +1,83 @@
-﻿using System.Diagnostics;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace OS
 {
     class Program
     {
-        static void Main(string[] args)
+        static string path;
+
+        static Dictionary<string, List<string>> folderTypes = new Dictionary<string, List<string>> {
+           { "Text", new List<string> { ".txt", ".doc",".pdf"} },
+           { "Images", new List<string> { ".png", ".jpeg",".ico",".jpg" } },
+           { "Programs", new List<string> { ".exe" } },
+           { "Videos", new List<string> { ".avi", ".mp4",".gif" } },
+           { "Music", new List<string> { ".mp3", ".wav",".ogg" } },
+           { "Archive", new List<string> { ".zip", ".rar" } },
+           { "Code", new List<string> { ".cs", ".html",".php",".cpp",".js" } }
+
+        };
+        static void CreateFolders()
         {
-            if (args.Length >=1)
+            //          Создание папок
+            for (int i = 0; i < folderTypes.Count; i++)
             {
-                FileInfo fi = new FileInfo(args[0]);
+                if (Directory.Exists($"{path + folderTypes.ElementAt(i).Key}") == false)
+                    Directory.CreateDirectory($"{path + folderTypes.ElementAt(i).Key}");
+            }
+            Directory.CreateDirectory($"{path}Other");
+        }
 
-                Console.WriteLine($"Full path:\t\t{ fi.FullName}");
-                Console.WriteLine($"Creation Time:\t\t{ fi.CreationTime}");
-                Console.WriteLine($"Last Access Time:\t{ fi.LastAccessTime}");
-                Console.WriteLine($"Last Write Time:\t{ fi.LastWriteTime}");
-                Console.WriteLine($"Size (Bytes):\t\t{ fi.Length}");
-                Console.WriteLine($"Is Read Only:\t\t{ fi.IsReadOnly}");
+        static void FileSort()
+        {
+            //       Запись всех файлов в список
+            List<string> allFiles = Directory.GetFiles(path).Select(Path.GetFileName).ToList();
 
-                if (args.Length == 2 && args[1] == "--open")
+            //        Сортировка файлов
+            foreach (var item in folderTypes)
+            {
+                foreach (var innerItem in item.Value)
                 {
-                    Process.Start( new ProcessStartInfo($"{args[0]}" ) { UseShellExecute = true });
+                    allFiles.Where(x => x.Contains(innerItem)).ToList().ForEach(y => { File.Move($"{path + y}", $"{path + item.Key}\\{y}"); });
+                   
                 }
             }
+
+            //        Перемещение отавшихся файлов в директорию "Другое"
+            allFiles = Directory.GetFiles(path).Select(Path.GetFileName).ToList();
+            allFiles.ToList().ForEach(y => { File.Move($"{path + y}", $"{path}Other\\{y}"); });
+
+            //       Удаление пустых директорий 
+            List<string> dirs = Directory.GetDirectories(path).ToList();
+            foreach (var item in dirs)
+            {
+                if (Directory.GetFiles(item).Length==0)
+                    Directory.Delete(item);
+            }
+
+            Console.WriteLine("\n\nСортировка выполнена");
         }
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                path = args[0];
+                path += "\\";
+
+                if (Directory.Exists(path))
+                {
+                    CreateFolders();
+                    FileSort();
+                }
+                else
+                    Console.WriteLine("This directory does not exist");
+            }
+
+            Console.Read();
+        }
+
     }
 }
